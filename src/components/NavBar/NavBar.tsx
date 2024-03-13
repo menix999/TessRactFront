03ead3dfd3cbed34b8164645a2e89362a6f2b7f5
Damian Smoliński <damian.smolinski@codeagency.pl>
2024-03-13 22:41:21 +0100

    Prepare main page mobile

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -18,7 +18,7 @@ const NavBar = ({ translation, locale }: INavBar) => {
 
   return (
     <div className='h-16'>
-      <header className='flex h-16 shadow-xl w-full items-center px-8 fixed z-50 top-0 bg-white'>
+      {/* <header className='flex h-16 shadow-xl w-full items-center px-8 fixed z-50 top-0 bg-white'>
         <div className='w-3/4 flex justify-center'>
           <CustomLink href='/' locale={locale}>
             <div className='flex items-center w-full'>
@@ -40,6 +40,41 @@ const NavBar = ({ translation, locale }: INavBar) => {
           <Link href={routes.cart} className='flex items-center gap-5 cursor-pointer min-w-32'>
             <CartIcon /> {translation.cart}
           </Link>
+        </div>
+      </header> */}
+
+      <header className='flex flex-col lg:flex-row h-24 lg:h-16 shadow-xl w-full items-center px-8 fixed z-10 top-0 bg-white'>
+        <div className='flex lg:contents'>
+          <div className='w-3/4 flex justify-center order-1'>
+            <CustomLink href='/' locale={locale}>
+              <div className='flex items-center w-full'>
+                <div className='w-8 h-8 lg:w-14 lg:h-12'>
+                  {/* <TessRactLogo />
+                  <TessRactLogoResize width='60' height='54' /> */}
+                </div>
+                <span className='text-2xl ml-3 font-extrabold text-main-purple'>Tess-</span>
+                <span className='text-2xl font-extrabold'>ract</span>
+              </div>
+            </CustomLink>
+          </div>
+          <div className='flex w-full justify-center gap-4 order-3'>
+            <div className='flex items-center gap-2 w-16 ml-4'>
+              <ChangeLanguage locale={locale} />
+              <span>{locale.toUpperCase()}</span>
+            </div>
+            <div className='flex items-center gap-5 cursor-pointer w-36 whitespace-nowrap'>
+              <div className='w-8 h-8'>
+                <PersonIcon />
+              </div>
+              {translation.signIn}
+            </div>
+            <div className='flex items-center gap-5 cursor-pointer min-w-32'>
+              <CartIcon /> {translation.cart}
+            </div>
+          </div>
+        </div>
+        <div className='w-full max-w-lg order-2 h-full flex items-center justify-center'>
+          <SearchBar placeholder={translation.search} />
         </div>
       </header>
     </div>
